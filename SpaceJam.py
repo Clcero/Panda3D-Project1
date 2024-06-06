a6a0f9c39bd1d5c8035bf460fd376c993b70cb51
Luke Classen <lukeyclassen@gmail.com>
2024-06-06 19:18:26 -0400

    Random Planet Spawning, Space Station
    Planets spawn at "random" positions within a range, and space station spawns near origin

--- SpaceJam.py
+++ SpaceJam.py
@@ -18,21 +18,43 @@ class MyApp(ShowBase):
         self.accept('escape', self.quit)
 
     def SetupScene(self):
-        '''Load universe and planet'''
+        '''Load universe, space station, player, then randomly place the planets.'''
+
+        # Universe
         self.Universe = self.loader.loadModel("./Assets/Universe/Universe.x")
         self.Universe.reparentTo(self.render)
         self.Universe.setScale(15000)
-        
-        self.Planet1 = self.loader.loadModel("./Assets/Planets/protoPlanet.x")
-        self.Planet1.reparentTo(self.render)
-        self.Planet1.setPos(random.randint(100, 150), random.randint(3000, 5000), random.randint(50, 75)) 
-        self.Planet1.setScale(350)
+        uniTex = self.loader.loadTexture("./Assets/Universe/Universe.jpg")
+        self.Universe.setTexture(uniTex, 1)
 
-        tex = self.loader.loadTexture("./Assets/Universe/Universe.jpg")
-        tex2 = self.loader.loadTexture("./Assets/Universe/Universe.jpg")
-        self.Universe.setTexture(tex, 1)
-        self.Planet1.setTexture(tex2, 1)
-        
+        # Space Station
+        self.SpaceStation = self.loader.loadModel("./Assets/Space Station/station.glb")
+        self.SpaceStation.reparentTo(self.render)
+        stationTex = self.loader.loadTexture("./Assets/Space Station/Metal.jpg")
+        self.SpaceStation.setTexture(stationTex, 1)
+        self.SpaceStation.setPos(75, 500, 100)
+
+        # self.player = self.loader.loadModel.(".")
+
+        # Path dictionary
+        planets = [
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Mars.jpg"},
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Purple.png"},
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Sand.png"},
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Tiled.jpg"},
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Wicker.jpg"},
+            {"model_path": "./Assets/Planets/protoPlanet.x", "texture_path": "./Assets/Planets/Rock.jpg"}
+        ]
+
+        # Spawn planets at random positions
+        for i, planet_spec in enumerate(planets):
+            planet = self.loader.loadModel(planet_spec["model_path"])
+            planet.setTexture(self.loader.loadTexture(planet_spec["texture_path"]), 1)
+            planet.setPos(random.randint(-2000, 8000), random.randint(3000, 6000), random.randint(350, 2550))
+            planet.setScale(350)
+            planet.reparentTo(self.render)
+            setattr(self, f"Planet{i+1}", planet)
+
 
     # Prepare message if server wants to quit.
     def quit(self):
